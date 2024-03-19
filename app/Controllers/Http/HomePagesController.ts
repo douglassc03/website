39a1807c17a4cb25af7527b5e24720ad41052f6f
Douglas <therayjuss@gmail.com>
2024-03-18 23:32:55 -0300

    fix: homepage route

---
+++ app/Controllers/Http/HomePagesController.ts
@@ -0,0 +1,9 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class HomePagesController {
+
+    public async homepage({view}: HttpContextContract){
+        return view.render('documents.homepage')
+    }
+
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,22 +1,4 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer''
-|
-*/
+
 
 import Route from '@ioc:Adonis/Core/Route'
 //import UsersController from 'App/Controllers/Http/UsersController'
@@ -25,18 +7,17 @@ Route.get('/users', async ({ view }) => {
   return view.render('documents.index')
 })  
 
-Route.group(() => {
+/*Route.group(() => {
   Route.get('/', 'UsersController.index').as('index')
   Route.get('/:id', 'UsersController.show').as('show')
   Route.post('/', 'UsersController.store').as('store')  
-})
+})*/
 
 Route.get('/users/:id', 'UsersController.show')
-
-
 //Route.get('/users','documents.index')
 
 Route.get('/homepage', async ({ view }) => {
   return view.render('documents.homepage')
-})
+})  
+
 
